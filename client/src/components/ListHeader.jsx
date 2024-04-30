f328f62399cd0a2508b121948be4121c585d8c9d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-04-30 17:48:55 -0400

    Add buttons to list header

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -3,6 +3,10 @@ const ListHeader = ({ listName }) => {
     return (
       <div className="list-header">
         <h1>{listName}</h1>
+        <div className="button-container">
+          <button className="create">ADD NEW</button>
+          <button className="signout" onClick={signOut}>SIGN OUT</button>
+        </div>
       </div>
     )
 }
