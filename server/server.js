23299b03f690acfc8fa75cd0708c1ee99b2071f0
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-17 18:45:44 -0400

    Refactor server.js to add GET /todos endpoint

--- server/server.js
+++ server/server.js
@@ -2,6 +2,14 @@ const PORT = process.env.PORT || 8000
 const express = require('express')
 const app = express()
 
+// Get all TODOS ...
 
+app.get('/todos', async (req, res) => {
+    try {
+        // await
+    } catch (err) {
+        console.error(error)
+    }
+})
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
