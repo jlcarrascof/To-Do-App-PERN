b8cf12fec7f6f0499bf3deab8aa20e9111d8aee4
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-17 18:43:26 -0400

    Refactor server.js to remove unnecessary code

--- server/server.js
+++ server/server.js
@@ -2,8 +2,6 @@ const PORT = process.env.PORT || 8000
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello!')
-})
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
