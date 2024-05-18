20a377317dadcacd12d26cb61c6caa101aaf51bc
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-17 23:12:33 -0400

    refactor: Add async/await to fetch data in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,16 @@ import ListHeader from "./components/ListHeader"
 
 const App = () => {
 
+  const getData = async () => {
+    try {
+      const response = await fetch(`http://localhost:8000/todos/${userEmail}`)
+      const json = await response.json()
+      console.log(json)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div className="app">
       <ListHeader listName = {'🏝️ Holiday tick list'} />
