d1b8c7beeabac184607bf5f099fb86b8fa967fef
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-17 21:48:25 -0400

    chore: Update database configuration to use environment variables

--- server/db.js
+++ server/db.js
@@ -2,11 +2,11 @@ const Pool = require('pg').Pool
 require('dotenv').config()
 
 const pool = new Pool({
-    user: process.env.USERNAME,
-    password: process.env.PASSWORD,
-    host: process.env.HOST,
-    port: process.env.DBPORT,
-    database: 'todoapp'
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME
 })
 
 module.exports = pool
