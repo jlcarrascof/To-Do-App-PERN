1bd877f5f765e38b6ca085ce016700974a921e10
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-17 21:45:54 -0400

    chore: Update database configuration to use environment variables

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     user: process.env.USERNAME,
     password: process.env.PASSWORD,
     host: process.env.HOST,
-    port: process.env.DB_PORT,
+    port: process.env.DBPORT,
     database: 'todoapp'
 })
 
