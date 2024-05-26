20b039a17b705818fb7a8eb0e03b84592271fc6d
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-26 19:29:32 -0400

    Updating

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,9 +1,22 @@
-const ListItem = ({ task }) => {
+import TickIcon from './TickIcon'
+import ProgressBar from './ProgressBar'
 
+const ListItem = ({ task }) => {
     return (
-      <div className="list-item">
-        <p>{task.title}</p>
-      </div>
+      <li className="list-item">
+
+        <div className="info-container">
+          <TickIcon />
+          <p className="task-title">{task.title}</p>
+          <ProgressBar />
+        </div>
+
+        <div className="button-container">
+          <button className="edit">EDIT</button>
+          <button className="delete">DELETE</button>
+        </div>
+
+      </li>
     )
 }
 
