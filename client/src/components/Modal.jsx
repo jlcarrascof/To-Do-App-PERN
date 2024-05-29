f9afee01a8241e1a28e94a11668047669f784c56
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-29 18:58:58 -0400

    refactor: Update Modal component to display mode and close button

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,8 +1,14 @@
 const Modal = () => {
+
+    const mode = "edit"
+
     return (
       <div className="overlay">
         <div className="modal">
-
+          <div>
+            <h3>Let's {mode} you task</h3>
+            <button>X</button>
+          </div>
         </div>
       </div>
     )
