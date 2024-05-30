735a6d97092db89243e53c5564c78f311b759d32
jlcarrascof <entrenamientospuntonet@gmail.com>
2024-05-29 20:53:32 -0400

    feat: Add range input for progress tracking in Modal component

--- client/src/components/Modal.jsx
+++ client/src/components/Modal.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 const Modal = () => {
 
     const mode = "edit"
@@ -23,9 +25,11 @@ const Modal = () => {
               onChange={handleChange}
             />
             <br />
+            <label for="range">Drag to select your current progress</label>
             <input
               required
               type="range"
+              id="range"
               min="0"
               max="100"
               name="progress"
